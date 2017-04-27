73fe958354d275be179f17b91817cc9006d499dc
JordanPag <JordanPag@users.noreply.github.com>
2017-04-27 12:56:57 -0400

    Update monster.js

--- monster.js
+++ monster.js
@@ -1,1 +1,13 @@
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
 
+var yDistRaw = Math.floor(place/32) - Math.floor(mPlace/32);
+var xDistRaw = place%32 - mplace%32;
+var yDist = Math.abs(yDistRaw);
+var xDist = Math.abs(xDistRaw);
