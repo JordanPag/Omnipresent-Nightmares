962b6a8c45c7aa12a206dfd2689827f29382bdf9
JordanPag <JordanPag@users.noreply.github.com>
2017-05-30 09:52:51 -0400

    Update monster.js

--- monster.js
+++ monster.js
@@ -11,16 +11,20 @@ var yDistRaw = Math.floor(place/32) - Math.floor(mPlace/32);
 var xDistRaw = place%32 - mPlace%32;
 var yDist = Math.abs(yDistRaw);
 var xDist = Math.abs(xDistRaw);
+var touching = false;
 
 function move() {
   if(yDist>=xDist){
     console.log("up/down");
   }
+  
+  if(mPlace == place){
+    touching = true
+  }
 }
 
 window.onload = function(){
-  var x = 1;
-  while(x==x){
+  while(touching == false){
     move();
     sleep(250);
     var yDistRaw = Math.floor(place/32) - Math.floor(mPlace/32);
@@ -28,5 +32,6 @@ window.onload = function(){
     var yDist = Math.abs(yDistRaw);
     var xDist = Math.abs(xDistRaw);
   }
+  AddRoom("lose");
 }
 
