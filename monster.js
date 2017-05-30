a2d8ebd372bd1eac1ee0e55d34ee31987ab1361a
JordanPag <JordanPag@users.noreply.github.com>
2017-05-30 09:58:33 -0400

    Update monster.js

--- monster.js
+++ monster.js
@@ -24,14 +24,17 @@ function move() {
 }
 
 window.onload = function(){
-  while(touching == false){
-    move();
-    sleep(250);
+  for(var i=0; i<960; i++;){
     var yDistRaw = Math.floor(place/32) - Math.floor(mPlace/32);
     var xDistRaw = place%32 - mPlace%32;
     var yDist = Math.abs(yDistRaw);
     var xDist = Math.abs(xDistRaw);
+    move();
+    sleep(250);
+    if(touching == true){
+      AddRoom("lose");
+      break;
+    }
   }
-  AddRoom("lose");
 }
 
