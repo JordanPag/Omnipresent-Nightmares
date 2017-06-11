f2516e9d21a359711c2b1d36028dc8f5d1555b05
JordanPag <JordanPag@users.noreply.github.com>
2017-06-11 15:36:51 -0400

    Create monster.js

--- monster.js
+++ monster.js
@@ -1,19 +1,10 @@
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
-      break;
-    }
-  }
-}
-
 var yDistRaw = Math.floor(place/32) - Math.floor(mPlace/32);
 var xDistRaw = place%32 - mPlace%32;
 var yDist = Math.abs(yDistRaw);
 var xDist = Math.abs(xDistRaw);
 var touching = false;
 
-function move() {
+function check() {
   if(yDist>=xDist){
     console.log("up/down");
   }
@@ -23,18 +14,18 @@ function move() {
   }
 }
 
-window.onload = function(){
-  for(var i=0; i<960; i++){
-    var yDistRaw = Math.floor(place/32) - Math.floor(mPlace/32);
-    var xDistRaw = place%32 - mPlace%32;
-    var yDist = Math.abs(yDistRaw);
-    var xDist = Math.abs(xDistRaw);
-    move();
-    if(touching == true){
-      AddRoom("lose");
-      break;
-    }
-    sleep(250);
-  }
-}
 
+function move() {
+  var yDistRaw = Math.floor(place/32) - Math.floor(mPlace/32);
+  var xDistRaw = place%32 - mPlace%32;
+  var yDist = Math.abs(yDistRaw);
+  var xDist = Math.abs(xDistRaw);
+  check();
+  if(touching == true){
+    AddRoom("lose");
+    break;
+  }
+  setTimeout(function(){
+    move();
+  }, 250);
+}
