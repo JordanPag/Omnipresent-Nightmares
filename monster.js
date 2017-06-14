a2a5fe26a71cd4f388fedda6ab02073c96799428
JordanPag <JordanPag@users.noreply.github.com>
2017-06-14 11:44:03 -0400

    Create monster.js

--- monster.js
+++ monster.js
@@ -23,6 +23,8 @@ function move() {
   var xDistRaw = place%32 - mPlace%32;
   var yDist = Math.abs(yDistRaw);
   var xDist = Math.abs(xDistRaw);
+  console.log(yDist);
+  console.log(xDist);
   check();
   if(touching == true){
     AddRoom("lose");
