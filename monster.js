16a4186acf38757898c5fd101c868c67aa20d1a0
JordanPag <JordanPag@users.noreply.github.com>
2017-06-15 18:17:20 -0400

    Create monster.js

--- monster.js
+++ monster.js
@@ -4,31 +4,45 @@ var yDist = Math.abs(yDistRaw);
 var xDist = Math.abs(xDistRaw);
 var touching = false;
 
-function check() {
+function direc() {
   var yDistRaw = Math.floor(place/32) - Math.floor(mPlace/32);
   var xDistRaw = place%32 - mPlace%32;
   var yDist = Math.abs(yDistRaw);
   var xDist = Math.abs(xDistRaw);
   console.log(yDist);
   console.log(xDist);
-  if(yDist>=xDist){
-    console.log("up/down");
-  }
-  else {
-    console.log("left/right");
-  }
-  
   if(mPlace == place){
     touching = true
   }
+  if(yDist>=xDist){
+    return("up/down");
+  }
+  else {
+    return("left/right");
+  }
 }
 
+function check(id){
+  var yesorno = document.getElementById(id).className;
+  if(yesorno=="floor"||yesorno=="player"){
+    yesorno = true;
+  } else {
+    yesorno = false;
+  }
+}
 
 function move() {
-  check();
+  var direction = direc();
+  var up = check(mPlace-32);
+  var down = check(mPlace+32);
+  var right = check(mPlace+1);
+  var left = check(mPlace-1);
   if(touching == true){
     AddRoom("lose");
     return;
+  }
+  if(direction=="up/down"){
+    if
   }
   setTimeout(function(){
     move();
